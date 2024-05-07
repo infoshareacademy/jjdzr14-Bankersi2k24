d015205aefb5d234f464dd5afcca1e8d278aba30
Ryszard Czajkowski <ryszardiczajkowski@gmail.com>
2024-05-07 19:47:46 +0200

    SubMenu

--- Bankesi2k24/src/main/java/pl/isa/App.java
+++ Bankesi2k24/src/main/java/pl/isa/App.java
@@ -1,5 +1,7 @@
 package pl.isa;
 
+import pl.isa.view.SubMenu;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SubMenu.subMenu();
     }
 }
--- Bankesi2k24/src/main/java/pl/isa/view/SubMenu.java
+++ Bankesi2k24/src/main/java/pl/isa/view/SubMenu.java
@@ -5,28 +5,26 @@ import pl.isa.model.BankAccount;
 import java.util.Scanner;
 
 public class SubMenu {
-    public static void main(String[] args) {
+    public static void subMenu() {
         String newLine = System.lineSeparator();
-        System.out.println("wybierz opcje:"+newLine+"1.Account balance"+newLine+"2.Add a new account"+newLine+"3.User date"+newLine+"4.Account list");
+        System.out.println("select options:"+newLine+"1.Account balance"+newLine+"2.Add a new account"+newLine+"3.User date"+newLine+"4.Account list");
         Scanner sc = new Scanner(System.in);
         int opcje = sc.nextInt();
         if (opcje == 1) {
-            System.out.println();
+            System.out.println("1.Account balance");
 
         } else if (opcje == 2) {
 
-
-
-            System.out.println();
+            System.out.println("2.Add a new account");
         } else if (opcje == 3) {
-            System.out.println();
+            System.out.println("3.User date");
 
         } else if (opcje == 4) {
-            System.out.println();
+            System.out.println("4.Account list");
 
 
         }else {
-            System.out.println(" Brak takiej opcji");
+            System.out.println(" NO SUCH OPTION ");
 
 
     }
