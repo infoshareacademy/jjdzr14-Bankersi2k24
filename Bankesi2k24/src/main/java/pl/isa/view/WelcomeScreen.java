da946b2576c9653f0da8b71dc9e8413acd62eb32
Ryszard Czajkowski <ryszardiczajkowski@gmail.com>
2024-05-21 22:29:53 +0200

    WelcomeScren Run

--- Bankesi2k24/src/main/java/pl/isa/view/WelcomeScreen.java
+++ Bankesi2k24/src/main/java/pl/isa/view/WelcomeScreen.java
@@ -6,14 +6,18 @@ import java.util.Scanner;
 
 //jira task: https://jira.is-academy.pl/browse/JJDZR14BA-3
 public class WelcomeScreen {
+    public static void main(String[] args) {
+        WelcomeScreen welcomeScreen = new WelcomeScreen();
+        User user = welcomeScreen.registrationScreen();
+    }
 
 
     public void loginScreen() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your login..");                               //Ryszard
+        System.out.println("Enter your login..");
         String login = scanner.next();
-        System.out.println("Enter your password..");                             //Ryszard
+        System.out.println("Enter your password..");
         String password = scanner.next();
 
         if (checkloginScreen(login, password)) {
@@ -25,49 +29,38 @@ public class WelcomeScreen {
         System.out.println("Welcome!!!  " + login);
     } // Zaloguj -> podaj login, haslo z zawartą metodą checkloginScreen
 
-    public void registrationScreen() {
+
+    public User registrationScreen() {
         Scanner scanner = new Scanner(System.in);
         User user = new User();
-
-        System.out.println("Enter your name...");
-        String name = scanner.next();
-        user.setName(name);
-
-        System.out.println("Enter your last name...");
-//        setLastName(scanner.next());
-        String lastName = scanner.next();
-        user.setLastName(lastName);
-
-//        if (getName().length() == 0 || getLastName().length() == 0) {
-//            System.out.println("Why empty phrases? Please, try again");
-//        } NIE WIEM DLACZEGO ALE TO NIE DZIAŁA;/
-
-        if (specialCharacters(name) || badNumbers(name) || specialCharacters(lastName) || badNumbers(lastName)) {
-            System.out.println("Invalid input. Please enter only letters.");
-            return;                                                                        //Ryszard po zmianach
-
-//        if (specialCharacters(getName()) || badNumbers(getName()) || specialCharacters(getLastName()) || badNumbers(getLastName())) {
-//            System.out.println("Invalid input. Please enter only letters.");
-//            return; //Ryszard
-        }
-        System.out.println("Enter your new login...");
+        String name = "";
+        String lastName = "";
+        do {
+            System.out.println("Enter your name...");
+            name = scanner.next();
+            user.setName(name);
+        } while (specialCharacters(name) || badNumbers(name));
+        do {
+            System.out.println("Enter your last name...");
+            lastName = scanner.next();
+            user.setLastName(lastName);
+        } while (specialCharacters(lastName) || badNumbers(lastName));
+        System.out.println("Enter login..");
         String login = scanner.next();
         user.setLogin(login);
-//        setRegLogin(scanner.next());                                       // to bylo Jacka
-        String regLogin = scanner.next();                                    //Ryszard
-        System.out.println("Enter your new password...");
+
+        System.out.println("Enter password..");
         String password = scanner.next();
         user.setPassword(password);
+        return user;
 
-//        setRegPassword(scanner.next());                                 // to bylo Jacka
-//        System.out.println("New user:" + getName().toUpperCase() + " " + getLastName().toUpperCase());  to bylo Jacka
-        System.out.println("New user:" + name.toUpperCase() + " " + lastName.toUpperCase()); // Rysard
     } // Zarejestuj -> Imie, nazwisko, login, hasło. Tutaj mam problem z IF.
 
     public boolean specialCharacters(String special) {
         String specialCharacters = "!@#$%^&*()_+-=[]{}|;':,.<>?";
         for (char s : special.toCharArray()) {
             if (specialCharacters.contains(String.valueOf(s))) {
+                System.out.println("No special characters.");
                 return true;
             }
         }
@@ -77,19 +70,17 @@ public class WelcomeScreen {
     public boolean badNumbers(String numbers) {
         for (char n : numbers.toCharArray()) {
             if (Character.isDigit(n)) {
+                System.out.println("No digits.");
                 return true;
             }
         }
         return false;
     }
 
-    public boolean checkloginScreen(String login, String password) {       //Ryszard
+    public boolean checkloginScreen(String login, String password) {
+        return false;
 
-        if (login.equals(regLogin()) && password.equals(getPassword())) {
-            return true;
-        } else {
-            return false;
-        }
+
     }
 }
 
