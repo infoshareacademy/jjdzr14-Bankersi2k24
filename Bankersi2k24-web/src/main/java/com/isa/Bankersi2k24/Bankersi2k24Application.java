62f82dde55f48e45d278a7622ed790cf61a293dd
piotrek-piotrek-dev <piotrek.piotrowski.dev@gmail.com>
2024-06-09 13:32:41 +0200

    creating project stub, nothimg more than generated from spring initializer

---
+++ Bankersi2k24-web/src/main/java/com/isa/Bankersi2k24/Bankersi2k24Application.java
@@ -0,0 +1,13 @@
+package com.isa.Bankersi2k24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankersi2k24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bankersi2k24Application.class, args);
+	}
+
+}
---
+++ Bankersi2k24-web/src/test/java/com/isa/Bankersi2k24/Bankersi2k24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.Bankersi2k24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bankersi2k24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
