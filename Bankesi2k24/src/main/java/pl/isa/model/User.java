f985739c84e776e941c49c9c84246e9fec2cf24c
piotrek-piotrek-dev <piotrek.piotrowski.dev@gmail.com>
2024-06-09 12:17:12 +0200

    adding equals & hasCode to User class

--- Bankesi2k24/src/main/java/pl/isa/model/User.java
+++ Bankesi2k24/src/main/java/pl/isa/model/User.java
@@ -92,5 +92,16 @@ public class User {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId()) && Objects.equals(getName(), user.getName()) && Objects.equals(getLogin(), user.getLogin()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getCreationDate(), user.getCreationDate());
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getLogin(), getEmail(), getCreationDate());
+    }
 }
