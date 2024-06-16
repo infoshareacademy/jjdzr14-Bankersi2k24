bf96cca0526cf8ae401d7b392fff53eb71960fa4
Ryszard Czajkowski <ryszardiczajkowski@gmail.com>
2024-06-16 12:50:12 +0200

    Add Menu and LoginController.

--- Bankersi2k24-web/src/main/java/com/isa/Bankersi2k24/controller/LoginController.java
+++ Bankersi2k24-web/src/main/java/com/isa/Bankersi2k24/controller/LoginController.java
@@ -1,13 +1,9 @@
-package com.isa.Bankersi2k24;
+package com.isa.Bankersi2k24.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class LoginController {
-    @GetMapping("/")
     public String viewHomePage() {
         return "index";
     }
